24acaccae1a0cfdae89a82a77cc73e54d22d4c7a
asya2320 <64478347+asya2320@users.noreply.github.com>
2022-11-07 18:48:29 +0300

    feat: update js file

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,38 @@
+const fs = require("fs");
+const fsPromises = fs.promises;
+const path = require("path");
+const copyFile = fsPromises.copyFile;
+
+(function copyDir() {
+  fs.mkdir(
+    path.join(__dirname, "files-copy"),
+    {
+      recursive: true,
+    },
+    (err) => {
+      if (err) {
+        throw err;
+      }
+    }
+  );
+  fsPromises.readdir(path.join(__dirname, "files-copy")).then((filesCopy) => {
+    if (filesCopy.length != 0) {
+      for (let j = 0; j < filesCopy.length; j++) {
+        fs.unlink(
+          path.join(__dirname, "files-copy", filesCopy[j]),
+          function (err) {
+            if (err) return console.log(err);
+          }
+        );
+      }
+    }
+  });
+
+  fsPromises.readdir(path.join(__dirname, "files")).then((files) => {
+    for (let i = 0; i < files.length; i++) {
+      let filePath = path.join(__dirname, "files", files[i]);
+      copyFile(filePath, path.join(__dirname, "files-copy", files[i]));
+    }
+  });
+})();
 
