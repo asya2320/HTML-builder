540285e47ed7af97569fe7cbff965c2829c1a8fd
asya2320 <64478347+asya2320@users.noreply.github.com>
2022-11-09 20:53:57 +0300

    feat: update js file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,11 +1,21 @@
 const fs = require("fs");
 const path = require("path");
-const stream =fs.createReadStream(path.resolve(__dirname, "text.txt"));
+const stream = fs.createWriteStream(path.resolve(__dirname, "text.txt"));
 
-stream.on("readable", ()=>{
-    const data = stream.read();
-    if(data){
-        console.log(data.toString());
-    }   
-})
+const { stdin, stdout, exit } = require("process");
 
+stdout.write("Text something...\n");
+
+stdin.on("data", (data) => {
+  if (data.toString().trim() == "exit") {
+    stdout.write("Good luck!");
+    exit();
+  }
+  stream.write(data);
+});
+
+//Сигнал прерывания (Ctrl+C) с терминала
+process.on("SIGINT", () => {
+  stdout.write("Good luck!");
+  process.exit();
+});
